19bd4fa6dfca8d03a9ba7ad4f2801327f9d8c951
snoe-lee <2470374084@qq.com>
2019-06-25 17:35:05 +0800

    修改路径地址

--- link/src/components/HelloWorld.vue
+++ link/src/components/HelloWorld.vue
@@ -43,7 +43,7 @@ export default {
   },
   methods: {
     getHomeInfo() {
-      axios.get('/api/index.json')
+      axios.get('/link/static/mock/index.json')
         .then(this.getHomeInfoSucc);
     },
     getHomeInfoSucc(res) {
