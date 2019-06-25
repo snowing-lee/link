d146480300ee91f05bac4e6f076225e785b21fab
snoe-lee <2470374084@qq.com>
2019-06-25 17:02:07 +0800

    信息修改

--- link/src/components/HelloWorld.vue
+++ link/src/components/HelloWorld.vue
@@ -1,113 +1,85 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+  <div class="all">
+    <div class="link">
+      <div class="title">{{ msg }}</div>
+      <div>
+        提示:
+        <div class="tip">{{ tip1 }}</div>
+      </div>
+
+      <div class="link-href">
+        <div  v-for="(item, index) of list" :key="index">
+          <h2>{{item.th}}</h2>
+          <ul>
+            <li><a class="aaa" :href="item.homeWork">作业上传</a></li>
+            <br>
+            <li><a class="aaa" :href="item.homeWorkGather">作业汇总</a></li>
+            <br>
+            <li><a class="aaa" :href="item.seatingChart">座位表</a></li>
+            <br>
+            <li><a class="aaa" :href="item.information">资料</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="foot">
+        <a :href="dormitory">宿舍安排</a>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'HelloWorld',
   data() {
     return {
-      msg: 'Welcome to Your Vue.js App',
+      tip1: '1,作业提交及作业汇总情况查看时,确认已连接局域网(网线或wangdao-meeting)',
+      msg: '资料汇总',
+      dormitory: '',
+      list: [],
     };
+  },
+  methods: {
+    getHomeInfo() {
+      axios.get('/api/index.json')
+        .then(this.getHomeInfoSucc);
+    },
+    getHomeInfoSucc(res) {
+      const res1 = res.data;
+      if (res1.ret && res1.data) {
+        const data1 = res1.data;
+        this.list = data1.list;
+        this.dormitory = data1.dormitory;
+      }
+    },
+  },
+  mounted() {
+    // 生命周期函数
+    this.getHomeInfo();
   },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+.link {
+  margin-left: 4rem;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+  .title {
+    margin-left: 5rem;
+    font-size: 200%;
+  }
+  .tip {
+    color: red;
+    margin-left: 5rem;
+    font-size: 80%;
+  }
+  .aaa {
+    line-height: 1rem;
+  }
+.foot {
+  margin-bottom: 5rem;
 }
 </style>
